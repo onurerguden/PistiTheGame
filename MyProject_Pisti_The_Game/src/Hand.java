56e0c0bac106879ad7029aa413bba776a57b7b2d
onurerguden <119923881+onurerguden@users.noreply.github.com>
2022-12-26 10:03:06 +0300

    Hand Class
    I finished the Hand class add some important methods like isnertCard, eraseCard...

--- MyProject_Pisti_The_Game/src/Hand.java
+++ MyProject_Pisti_The_Game/src/Hand.java
@@ -1,2 +1,43 @@
 public class Hand {
+    private Card cards[];
+    private int totalCardNumber;
+    private int handCardNumber;
+    private int cardIndex;
+
+
+    public Hand(int cardNumber){
+        this.totalCardNumber=cardNumber;
+        this.handCardNumber=0;
+        this.cardIndex=0;
+        cards=new Card[this.totalCardNumber];
+    }
+
+    public void insertCard(Card card){
+        if(cardIndex == totalCardNumber){
+            System.out.println("Cards are unable to give");
+        }
+        else{
+            cards[cardIndex]=card;
+            cardIndex++;
+            handCardNumber++;
+        }
+    }
+    public void eraseCard(int id){
+        cards[id]=null;
+        handCardNumber--;
+    }
+    public Card openCard(int id){
+        return cards[id];
+    }
+    public int getCardIndex(){
+        return cardIndex;
+    }
+    public int getHandCardNum(){
+        return this.handCardNumber;
+    }
+    public int getTotalCardNum(){
+        return this.totalCardNumber;
+    }
+
+
 }
