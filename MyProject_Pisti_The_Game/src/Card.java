845d99b3590f4bb2754ede1ed6e1c9add1dc0eca
onurerguden <119923881+onurerguden@users.noreply.github.com>
2022-12-26 09:44:07 +0300

    Card class
    i identify the integeers and Strings to use in deck class while creating the deck (set ,get methods...)

--- MyProject_Pisti_The_Game/src/Card.java
+++ MyProject_Pisti_The_Game/src/Card.java
@@ -1,2 +1,43 @@
 public class Card {
+    private int No;
+    private int Value;
+    private String Type;
+    private String Name;
+
+
+
+    Card(int No, int Value, String Type, String Name){
+        this.No=No;
+        this.Value=Value;
+        this.Type=Type;
+        this.Name=Name;
+    }
+
+
+    public int getNo() {
+        return No;
+    }
+
+    public void setNo(int no) {
+        No = no;
+    }
+
+    public int getValue() {
+        return Value;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String Anc(){
+        return "   "+Type+" "+Name +"\n Value: "+ Value+ "\n  No: "+(No+1) +"\n --------";
+    }
+
+
+
 }
